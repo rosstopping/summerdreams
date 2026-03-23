5165a1ad6785fe830a2155e6df5e707dce74981f
rosstopping <ross@digizu.co.uk>
2026-03-23 06:43:20 +0000

    wip

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.app>
-    <x-page-header image="{{ Vite::asset('resources/images/zante event packages 26.jpg') }}">
+    <x-page-header image="/images/zante event packages 26.jpg">
         <x-slot:title>
             {{ $page->title }}
         </x-slot>
